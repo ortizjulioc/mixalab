a5b61c66963ef2f30354c0a8bccd5d2e1860ae03
Vladimir Nuñez <black3xp@MacBook-Air-de-Vladimir.local>
2025-10-09 10:56:08 -0400

    [Add] - Se agrego el componente security pass

--- app/(dashboard-artists)/artists/home/page.jsx
+++ app/(dashboard-artists)/artists/home/page.jsx
@@ -1,7 +1,10 @@
+import CreatorSecurityPass from '@/app/components/CreatorSecurityPass'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <CreatorSecurityPass />
+    </div>
   )
 }
