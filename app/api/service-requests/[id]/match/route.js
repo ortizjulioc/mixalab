704c2322f49a58b1ef3685e812e0e0512fd9da77
Wilber Galvez <galvezwilber22@gmail.com>
2026-01-29 08:08:13 -0400

    [add] rutas para manejar mejor las request

--- app/api/service-requests/[id]/match/route.js
+++ app/api/service-requests/[id]/match/route.js
@@ -1,7 +1,7 @@
 import prisma from '@/utils/lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../../auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 /**
  * GET /api/service-requests/[id]/match
