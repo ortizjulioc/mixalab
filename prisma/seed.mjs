d9e56b957ef424be8881356ea72be4bbc3069b61
Anderson Frias <anderl.friaas@gmail.com>
2026-01-30 08:57:54 -0400

    update: coneccion a mysql lista

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -6,6 +6,7 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
--- utils/lib/prisma.js
+++ utils/lib/prisma.js
@@ -7,6 +7,7 @@ const globalForPrisma = global;
 if (!globalForPrisma.prisma) {
   const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
