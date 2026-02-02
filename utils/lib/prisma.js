40a2fb6bd28a38deb0d6a5ce416e1d274cecc776
Wilber Galvez <galvezwilber22@gmail.com>
2026-02-02 11:52:16 -0400

    [add] feat: configurar Prisma y agregar modelos para el módulo de proyectos

--- utils/lib/prisma.js
+++ utils/lib/prisma.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global;
 
