e740f081abaf96c3fa8a400c6a04e5fbed80b81f
JulioS21 <tuemail@example.com>
2026-02-18 09:33:28 -0400

    Fix migration

--- utils/lib/prisma.js
+++ utils/lib/prisma.js
@@ -1,7 +1,15 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
