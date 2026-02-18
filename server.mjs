2cb0de08bc41f461b8da3dad87e5cb2fbc3bf5e9
JulioS21 <tuemail@example.com>
2026-02-18 10:35:07 -0400

    Update server hostname logging

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME || "localhost";
+const hostname = "0.0.0.0";
 const port = process.env.PORT || 3000;
 
 global.io = null;
